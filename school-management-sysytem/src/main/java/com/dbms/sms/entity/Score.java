f3a50253f59609f6afe37b6e3b70016c7b36026e
Abhay Singh <abhay.singh.met21@itbhu.ac.in>
2023-10-03 23:29:26 +0530

    created entities except admin and RIC have not been applied yet

--- school-management-sysytem/src/main/java/com/dbms/sms/entity/Score.java
+++ school-management-sysytem/src/main/java/com/dbms/sms/entity/Score.java
@@ -9,12 +9,16 @@ import jakarta.persistence.*;
 @Table(name="scores")
 public class Score {
 	
+	@EmbeddedId scoreId id; 
+	
+	@Column(name="score")
+	private Long score;
+}
+class scoreId{
+	
 	@Column(name="studId")
 	private int studId;
 	
 	@Column(name="subjId")
 	private int subjId;
-	
-	@Column(name="score")
-	private Long score;
 }
